ceba282e53416b15f3b6e84df8662db99cbe7cfe
arizsalman <muhammadarizsalman@gmail.com>
2024-09-12 18:20:48 +0500

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {NextUIProvider} from '@nextui-org/react'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </StrictMode>,
 )
